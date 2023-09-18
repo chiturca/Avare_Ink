8fb9c0aa1f2beb5821b5ac58f50dc4bc31408076
Miray <sonmezmiray@gmail.com>
2023-09-18 13:47:43 +0300

    Feat: fix lg nav

--- app/components/navbar/Logo.js
+++ app/components/navbar/Logo.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import Avare from "../assets/avareink.svg";
 import logo from "../../assets/logo2.png";
 
 export default function Logo() {
@@ -10,7 +9,7 @@ export default function Logo() {
         href="/"
         rel="noopener noreferrer"
       >
-        <div className="relative flex place-items-center before:absolute before:h-[50px] before:w-[50px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[150px] after:w-[150px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <div>
           <Image
             src={logo}
             alt="AvareInk Logo"
--- app/components/navbar/Nav.js
+++ app/components/navbar/Nav.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
@@ -10,26 +11,37 @@ export const links = [
   { href: "/book", name: "Book" },
 ];
 export default function Nav() {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className="sticky top-0 z-10 w-full items-center justify-between font-mono text-sm lg:flex dark">
-      <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex drop-shadow-lg">
-        <nav className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <ul className="flex flex-row">
+    <>
+      <div className="flex sticky top-0 z-10 w-full items-center justify-between font-mono text-sm  dark">
+        <nav className="fixed left-0 top-0 flex w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 drop-shadow-lg">
+          <ul className="lg:flex md:hidden sm:hidden flex-wrap flex-row justify-between items-center w-full">
             <li>
               <Logo />
             </li>
-            {links.map((link) => (
-              <li
-                key={link.href}
-                className="m-7 text-sky-200 text-2xl text-shadow-[0 0 50px #bae6fd]"
-              >
-                <Link href={link.href}>{link.name}</Link>
-              </li>
-            ))}
+            <li>
+              <ul className="flex flex-row items-center">
+                {links.map((link) => (
+                  <li
+                    key={link.href}
+                    onClick={() => setActive(link.name)}
+                    className={`m-7 text-2xl text-shadow-[0 0 50px #bae6fd] ${
+                      active === link.name ? "text-sky-200" : "text-sky-500"
+                    }`}
+                  >
+                    <Link href={link.href}>{link.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <UserMenu />
+            </li>
           </ul>
         </nav>
       </div>
-      <UserMenu />
-    </div>
+    </>
   );
 }
