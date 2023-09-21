67314d05c4fc73354ab5b29be4ed2a8076fb8332
Miray <sonmezmiray@gmail.com>
2023-09-21 15:51:07 +0300

    Style: add footer pages and fix sm nav

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../navbar/Logo";
+import Link from "next/link";
+
+export const links = [
+  { href: "/about", name: "About" },
+  { href: "/privacypolicy", name: "Privacy Policy" },
+  { href: "/licensing", name: "Licensing" },
+  { href: "/contact", name: "Contact" },
+];
 
 export default function Footer() {
+  const [active, setActive] = useState("");
   return (
     <div className="sticky top-0 left-0 z-10 w-full justify-between font-mono text-sm dark">
       <footer className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit static rounded-xl border bg-gray-200 lg:p-2 drop-shadow-lg m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="sm:flex sm:items-center justify-between">
             <div className="flex flex-row">
               <Logo />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-3">
                 Avare Ink
               </span>
             </div>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                  Licensing
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:underline">
-                  Contact
-                </a>
-              </li>
+            <ul className="flex flex-wrap items-center my-6 text-sm font-medium text-gray-500 dark:text-gray-400">
+              {links.map((link) => (
+                <li
+                  key={link.href}
+                  onClick={() => setActive(link.name)}
+                  className={`mr-4 hover:underline md:mr-6 ${
+                    active === link.name ? "text-sky-200" : "text-sky-500"
+                  }`}
+                >
+                  <Link href={link.href}>{link.name}</Link>
+                </li>
+              ))}
             </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
---
+++ app/licensing/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Licensing() {
+  return <div>Licensing</div>;
+}
