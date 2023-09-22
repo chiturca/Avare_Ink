90868635dd9a61c115d5c9c44d88f442125f9262
Miray <sonmezmiray@gmail.com>
2023-09-22 13:47:05 +0300

    Feat: fix footer link active state w/usePathname

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Logo from "../navbar/Logo";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import Logo from "../navbar/Logo";
 
 export const links = [
   { href: "/about", name: "About" },
@@ -10,7 +10,7 @@ export const links = [
 ];
 
 export default function Footer() {
-  const [active, setActive] = useState("");
+  const pathname = usePathname();
   return (
     <div className="bottom-0 left-0 z-10 w-full justify-between font-mono text-sm dark">
       <footer className="static border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  rounded-xl border bg-gray-200 lg:p-2 drop-shadow-lg m-4">
@@ -24,14 +24,15 @@ export default function Footer() {
             </div>
             <ul className="flex flex-wrap items-center my-6 text-sm font-medium text-gray-500 dark:text-gray-400">
               {links.map((link) => (
-                <li
-                  key={link.href}
-                  onClick={() => setActive(link.name)}
-                  className={`mr-4 hover:underline md:mr-6 ${
-                    active === link.name ? "text-sky-200" : "text-sky-500"
-                  }`}
-                >
-                  <Link href={link.href}>{link.name}</Link>
+                <li key={link.href} className="mr-4 hover:underline md:mr-6">
+                  <Link
+                    className={
+                      pathname === link.href ? "text-sky-200" : "text-sky-500"
+                    }
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
                 </li>
               ))}
             </ul>
