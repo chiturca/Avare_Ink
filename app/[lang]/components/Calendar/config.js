6c79b50e7662755b76cf9630b3f83204f9cc876a
Miray <sonmezmiray@gmail.com>
2023-10-02 21:37:56 +0300

    Feat: change Cards order on Book

--- app/[lang]/components/Calendar/config.js
+++ app/[lang]/components/Calendar/config.js
@@ -1,13 +1,3 @@
 export const OPENINING_TIME = 10; //hours
 export const CLOSING_TIME = 18; //hours
 export const INTERVAL = 60; //minutes
-
-const tattooSizes = {
-  xsmall: { size: "XSmall", duration: "1 hour" },
-  small: { size: "Small", duration: "2 hours" },
-  medium: { size: "Medium", duration: "4 hours" },
-  large: { size: "Large", duration: "6 hours" },
-  extraLarge: { size: "Extra Large+", duration: "8 hours" },
-};
-
-export default tattooSizes;
