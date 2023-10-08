fd3ea10e253e19a6b208b0ac4a4d22a1399dff60
Miray <sonmezmiray@gmail.com>
2023-10-08 12:41:35 +0300

    Feat: fix translations to be open as selected on local

--- middleware.js
+++ middleware.js
@@ -1,7 +1,3 @@
-import { NextResponse } from "next/server";
-import { match as matchLocale } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-import { i18n } from "./i18n.config";
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
@@ -10,37 +6,7 @@ export default createMiddleware({
   defaultLocale: "tr",
 });
 
-function getLocale(request) {
-  const negotiatorHeaders = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  // @ts-ignore locales are readonly
-  const locales = i18n.locales;
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-
-  const locale = matchLocale(languages, locales, i18n.defaultLocale);
-  return locale;
-}
-
-export function middleware(request) {
-  const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url
-      )
-    );
-  }
-}
-
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
