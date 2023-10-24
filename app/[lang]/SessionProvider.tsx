a8ede60bc53410e3b2e7d1d85e2fd2603332f840
Miray <sonmezmiray@gmail.com>
2023-10-24 16:35:49 +0300

    Feat: hardcoded admin role only in nextauth

--- app/[lang]/SessionProvider.tsx
+++ app/[lang]/SessionProvider.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { SessionProvider as Provider } from "next-auth/react";
 
-export default function SessionProvider({ children }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function SessionProvider({ children }: Props) {
   return <Provider>{children}</Provider>;
 }
