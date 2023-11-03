f811387fad51d739ce87c6497bb23c00348e3b34
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2023-11-03 23:36:12 +0300

    Add map (#52)
    * Build: npm i leaflet react-leaflet and update map
    
    * Refactor: try to fix window is not defined deployment error
    
    * Refactor: add IsClientCtx for window error
    
    * Refactor: add useEffect-useState for windows error
    
    * Refactor: add dynamic ssr:false for map window error

--- app/[lang]/contact/page.js
+++ app/[lang]/contact/page.js
@@ -1,11 +1,15 @@
-import Map from "../components/Map/Map";
 import { getDictionary } from "../dictionaries";
+import dynamic from "next/dynamic";
+
+const NoSSR = dynamic(() => import("../components/Map/Leaflet"), {
+  ssr: false,
+});
 
 export default async function Contact({ params: { lang } }) {
   const { Contact } = await getDictionary(lang);
   return (
     <div className="flex flex-wrap flex-col lg:flex-row">
-      <Map />
+      <NoSSR />
       <div className="w-[30em] lg:ml-4">
         <h1>{Contact.directions}</h1>
       </div>
