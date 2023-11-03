5c46cc848549a6d678202dd5605874fc53067b40
Miray <sonmezmiray@gmail.com>
2023-11-03 17:46:38 +0300

    Build: npm i leaflet react-leaflet and update map

---
+++ app/[lang]/components/Map/Leaflet.js
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function Leaflet() {
+  return (
+    <div style={{ height: "30em", width: "30em" }}>
+      <MapContainer
+        center={[39.93707, 32.86386]}
+        zoom={20}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[39.93707, 32.86386]}>
+          <Popup>
+            <h3>Avare_Ink</h3>
+            <p>Pilavoğlu Han Çarşısı, Ankara, Türkiye</p>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
--- app/[lang]/contact/page.js
+++ app/[lang]/contact/page.js
@@ -1,11 +1,11 @@
-import Map from "../components/Map/Map";
+import Leaflet from "../components/Map/Leaflet";
 import { getDictionary } from "../dictionaries";
 
 export default async function Contact({ params: { lang } }) {
   const { Contact } = await getDictionary(lang);
   return (
     <div className="flex flex-wrap flex-col lg:flex-row">
-      <Map />
+      <Leaflet />
       <div className="w-[30em] lg:ml-4">
         <h1>{Contact.directions}</h1>
       </div>
