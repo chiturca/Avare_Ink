9abe3259cfa76f3e63e63c85bd2c8b1f2cc802de
Miray <sonmezmiray@gmail.com>
2023-11-03 14:00:41 +0300

    Refactor: add preflight on tailwind and delete unnucessary ui components

--- app/[lang]/components/ui/H1.js
+++
@@ -1,3 +0,0 @@
-export default function H1({ name }) {
-  return <h1 className="text-center text-3xl font-bold mb-2">{name}</h1>;
-}
--- app/[lang]/contact/page.js
+++ app/[lang]/contact/page.js
@@ -1,5 +1,4 @@
 import Map from "../components/Map/Map";
-import H1 from "../components/ui/H1";
 import { getDictionary } from "../dictionaries";
 
 export default async function Contact({ params: { lang } }) {
@@ -8,7 +7,7 @@ export default async function Contact({ params: { lang } }) {
     <div className="flex flex-wrap flex-col lg:flex-row">
       <Map />
       <div className="w-[30em] lg:ml-4">
-        <H1 name={Contact.directions} />
+        <h1>{Contact.directions}</h1>
       </div>
     </div>
   );
