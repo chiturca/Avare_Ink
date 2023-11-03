28333169f7875cad809921260ec5cdbffecb9aab
Miray <sonmezmiray@gmail.com>
2023-11-03 23:18:08 +0300

    Refactor: add dynamic ssr:false for map window error

--- app/[lang]/components/Map/Leaflet.js
+++ app/[lang]/components/Map/Leaflet.js
@@ -1,33 +1,23 @@
 "use client";
-import { useState, useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
+// https://nextjs.org/docs/messages/react-hydration-error
 export default function Leaflet() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
   return (
     <div style={{ height: "30em", width: "30em" }}>
-      {isClient ? (
-        <MapContainer
-          center={[39.93707, 32.86386]}
-          zoom={20}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[39.93707, 32.86386]}>
-            <Popup>
-              <h3>Avare_Ink</h3>
-              <p>Pilavoğlu Han Çarşısı, Ankara, Türkiye</p>
-            </Popup>
-          </Marker>
-        </MapContainer>
-      ) : (
-        ""
-      )}
+      <MapContainer
+        center={[39.93707, 32.86386]}
+        zoom={20}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[39.93707, 32.86386]}>
+          <Popup>
+            <h3>Avare_Ink</h3>
+            <p>Pilavoğlu Han Çarşısı, Ankara, Türkiye</p>
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
--- app/[lang]/contact/page.js
+++ app/[lang]/contact/page.js
@@ -1,11 +1,15 @@
-import Leaflet from "../components/Map/Leaflet";
 import { getDictionary } from "../dictionaries";
+import dynamic from "next/dynamic";
+
+const NoSSR = dynamic(() => import("../components/Map/Leaflet"), {
+  ssr: false,
+});
 
 export default async function Contact({ params: { lang } }) {
   const { Contact } = await getDictionary(lang);
   return (
     <div className="flex flex-wrap flex-col lg:flex-row">
-      <Leaflet />
+      <NoSSR />
       <div className="w-[30em] lg:ml-4">
         <h1>{Contact.directions}</h1>
       </div>
