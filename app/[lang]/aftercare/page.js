3d0abc7d5c9d30711e976cce65d537046fed3f3e
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-02-06 03:01:01 +0300

    Doc: add initial aftercare explanation

--- app/[lang]/aftercare/page.js
+++ app/[lang]/aftercare/page.js
@@ -5,6 +5,41 @@ export default async function Aftercare({ params: { lang } }) {
   return (
     <div>
       <h1>{Aftercare.title}</h1>
+      <ul className="list-disc m-8">
+        <li>
+          <p>{Aftercare.li1}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li2}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li3}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li4}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li5}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li6}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li7}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li8}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li9}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li10}</p>
+        </li>
+        <li>
+          <p>{Aftercare.li11}</p>
+        </li>
+      </ul>
     </div>
   );
 }
